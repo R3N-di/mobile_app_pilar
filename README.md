afdc5d4a63489751d9eab21277ccd2da61e194a9
R3N-di <rendiramadhanmars@gmail.com>
2023-08-19 11:31:58 +0700

    buat ulang

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_application_1
+# mobile_app_pilar
 
 A new Flutter project.
 
---
+++ android/app/src/main/kotlin/com/example/mobile_app_pilar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_app_pilar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
