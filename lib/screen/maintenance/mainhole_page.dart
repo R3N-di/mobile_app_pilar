b4a8259edacede89df1d9098c0323eaf89dde015
faizalagathon <faizalagathon06@gmail.com>
2023-08-25 09:48:41 +0700

    fix: mainhole page

--- lib/screen/maintenance/mainhole_page.dart
+++ lib/screen/maintenance/mainhole_page.dart
@@ -8,109 +8,43 @@ class MainholePage extends StatefulWidget {
   const MainholePage({super.key});
 
   @override
-  _MainholeDataTableState createState() => _MainholeDataTableState();
+  _MainholePageState createState() => _MainholePageState();
 }
 
-class _MainholeDataTableState extends State<MainholePage> {
-  late Future<List<MainholeModel>> _mainholeData;
-
+class _MainholePageState extends State<MainholePage> {
   @override
   void initState() {
-    _mainholeData = MainholeService().getMainhole();
+    data = MainholeService().getMainhole();
+    data.then((value) {
+      setState(() {
+        data2 = value;
+      });
+    });
     super.initState();
   }
 
+  late Future data;
+  List<MainholeModel> data2 = [];
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<MainholeModel>>(
-      future: _mainholeData,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(
-              color: primaryGreen,
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        } else {
-          final mainholeData = snapshot.data ?? [];
-          return SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: MyDataTableWidget(mainholeData: mainholeData),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('User Page'),
+        ),
+        body: data2.length == 0
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: Colors.black,
                 ),
-              ],
-            ),
-          );
-        }
-      },
-    );
+              )
+            : ListView.builder(
+                itemCount: data2.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(data2[index].lokasi),
+                  );
+                },
+              ));
   }
-}
-
-class MyDataTableWidget extends StatefulWidget {
-  final List<MainholeModel> mainholeData;
-
-  const MyDataTableWidget({required this.mainholeData, super.key});
-
-  @override
-  _MyDataTableWidgetState createState() => _MyDataTableWidgetState();
-}
-
-class _MyDataTableWidgetState extends State<MyDataTableWidget> {
-  final List<DataRow> _dataRows = [];
-  List<DataRow> _filteredRows = [];
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _ageController = TextEditingController();
-  TextEditingController _locationController = TextEditingController();
-
-  @override
-  void initState() {
-    _dataRows.addAll(widget.mainholeData.map((data) {
-      return DataRow(cells: [
-        DataCell(Text(data.lokasi)),
-        DataCell(Text(data.posisi.toString())),
-        DataCell(Text(data.bulan)),
-      ]);
-    }));
-    _filteredRows = _dataRows;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        children: [
-          DataTable(
-            columns: [
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('Age')),
-              DataColumn(label: Text('Location')),
-              DataColumn(label: Text('Actions')), // New column for actions
-            ],
-            rows: _filteredRows.map((row) {
-              return DataRow(
-                cells: [
-                  ...row.cells,
-                  DataCell(IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => {}, // Call remove function
-                  )),
-                ],
-              );
-            }).toList(),
-          ),
-        ],
-      ),
-    );
-  }
-  
 }
